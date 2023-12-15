223680525980801c7ab6836941de0fee68ce8fc2
Douglas Melo <douglasmnegri@gmail.com>
2023-12-15 12:40:02 -0300

    The grid gets colored when the mouse cursor flies over.

--- file.js
+++ file.js
@@ -1,9 +1,10 @@
-const container2 = document.querySelector('#container2');
+const container = document.querySelector('#container');
 
 function createBoxes(numBox) {
     for (let i = 0; i < numBox; i++) {
-      const row = container2.appendChild(document.createElement('div'));
+      const row = document.createElement('div');
       row.classList.add('row');
+      container.appendChild(row);
 
       for (let j = 0; j < numBox; j++) {
         const square = document.createElement('div');
@@ -15,3 +16,18 @@ function createBoxes(numBox) {
   
   
   createBoxes(50);
+
+  /* Learning .forEach()
+  const companyNames = ["Douglas", "Gabrielli", "Duda", "Dudinha", "Junior"];
+
+  companyNames.forEach((element, index) => {
+    let listQt = `There are ${index + 1} names in this list`;
+    console.log(listQt);
+  });
+*/
+  const square = document.querySelectorAll('.box');
+  square.forEach((div) => {
+    div.addEventListener('mouseover', () => { 
+      div.classList.add('paint');
+    });
+  });
